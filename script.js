bc35c38885d48bd2f323d985d70b119ed963903c
unknown <sharanya712pappu@gmail.com>
2021-12-10 11:40:41 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// var request=new XMLHttpRequest();
+// request.open('GET','https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json',true);
+// request.send();
+// request.onload=function(){
+//   var data=JSON.parse(request.response);
+//   console.log(data);
+//   for(var i in data){
+//     console.log(data[i].name);
+//   }
+//   for(var)
+// }
+// <!-- <script src="script.js"></script> -->
